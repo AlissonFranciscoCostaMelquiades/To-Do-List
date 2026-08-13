2f8bd22da0b2c1212a646daa99091b4226717d9c
Alisson Francisco Costa Melquiades <alisson.melquiades@aluno.senai.br>
2026-08-13 11:24:57 -0300

    Alterações aula 13/08

--- src/to/pkgdo/list/ToDoList.java
+++ src/to/pkgdo/list/ToDoList.java
@@ -4,6 +4,8 @@
  */
 package to.pkgdo.list;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Aluno
@@ -13,8 +15,37 @@ public class ToDoList {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+                
+        String[] Tarefas = new String[10];
+        
+        boolean[] concluidas = new boolean[10];
+        
+        int opcao = 0;
+        
+        while (opcao != 5) {
+            
+            System.out.println("\n====== LISTA DE TAREFAS ======");
+            System.out.println("1 - Adicionar Tarefa");
+            System.out.println("2 - Listar Tarefa");
+            System.out.println("3 - Concluir Tarefa");
+            System.out.println("4 - EXcluir Tarefa");
+            System.out.println("5 - Sair");
+            
+            System.out.println("\nEscolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.hasNextLine();
+            
+            switch (opcao) {
+                
+                case 1: 
+                    
+                    int posicaoLivre = -1;
+                    
+                    for (int i = 0; i < tarefas.length; i++) {
+                }
+            }
+        }
     }
-    
-}
+           
