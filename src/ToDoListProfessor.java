1e8786488bdfc4c59cf6f42f92a8449c34b02acc
Alisson Francisco Costa Melquiades <alisson.melquiades@aluno.senai.br>
2026-08-20 09:20:10 -0300

    Alterações aula 20/08

--- src/ToDoListProfessor.java
+++ src/ToDoListProfessor.java
@@ -88,8 +88,31 @@ public class ToDoListProfessor {
                         
                         System.out.println("Tarefa concluida com sucesso!");
                     }else{
-                        System.out.println("Tarefa inválida");
+                        System.out.println("Tarefa invalida!");
                     }
+                case 4:
+                    System.out.println("Digite o numero da tarefa que deseja excluir: ");
+                    int numeroExcluir = scanner.nextInt();
+                    
+                    int indiceExcluir = numeroExcluir - 1;
+                    
+                    if (indiceExcluir >= 0 && indiceExcluir < tarefas.length && tarefas[indiceExcluir] != null){
+                        
+                        tarefas[indiceExcluir] = null;
+                        concluidas[indiceExcluir] = false;
+                        
+                        System.out.println("Tarefa excluida com sucesso!");
+                        
+                    }else{
+                        System.out.println("Tarefa invalida!");
+                    }
+                    
+                    break;
+                case 5:
+                    System.out.println("Programa encerradp. Até mais!");
+                    
+                    break;
+                  
             }
         }
     }
